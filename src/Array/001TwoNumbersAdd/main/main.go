f65c597c3e73f0977f2711a206fdc76bd43911f8
Fcou <lyz672673107@gmail.com>
2019-12-19 08:59:18 +0800

    数组-找是否存在两个数之和是目标值

---
+++ src/Array/001TwoNumbersAdd/main/main.go
@@ -0,0 +1,52 @@
+/*
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+	// index 负责保存map[整数]整数的序列号
+	index := make(map[int]int, len(nums))
+
+	// 通过 for 循环，获取b的序列号
+	for i, b := range nums {
+		// 通过查询map，获取a = target - b的序列号
+		if j, ok := index[target-b]; ok {
+			// ok 为 true
+			// 说明在i之前，存在 nums[j] == a
+			return []int{j, i}
+			// 注意，顺序是j，i
+		}
+
+		// 把b和i的值，存入map
+		index[b] = i
+	}
+
+	return nil
+}
+
+func main() {
+	var nums []int = []int{1, 2, 4, 5, 44, 32, 6, 11, 23, 54}
+	target := 10
+
+	twonub := twoSum(nums, target)
+
+	fmt.Println(twonub)
+}
