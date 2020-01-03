6245535190470cb3d1d312ffb76cb6e3a9675435
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-01-03 11:53:25 +0800

    选择排序

---
+++ src/Sort/Selection Sort/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//SelectionSort选择排序，每次选出剩下元素中最小的，排在最前面
+func SelectionSort(nums []int) {
+	lens := len(nums)
+	for i := 0; i < lens; i++ {
+		temp := i
+		for j := i + 1; j < lens; j++ {
+			if nums[j] < nums[temp] {
+				temp = j
+			}
+		}
+		nums[i], nums[temp] = nums[temp], nums[i]
+
+	}
+
+}
+
+func main() {
+	nums := []int{3, 6, 1, 9, 2, 4, 8, 7, 5}
+
+	SelectionSort(nums)
+
+	fmt.Println(nums)
+}
