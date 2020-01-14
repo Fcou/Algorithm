88d3f1279bc1b70a286dd18f013850a2c89bb9d9
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-01-14 11:18:03 +0800

    循环队列

--- src/Queue/662CircularQueue/main.go
+++ src/Queue/662CircularQueue/main.go
@@ -50,7 +50,7 @@ circularQueue.Rear();  // 返回 4
 链接：https://leetcode-cn.com/problems/design-circular-queue
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
 
-package main
+/* package main
 
 // MyCircularQueue 结构体
 type MyCircularQueue struct {
@@ -95,22 +95,22 @@ func (m *MyCircularQueue) Front() int {
 }
 
 // Rear get the last item from the queue. */
-func (m *MyCircularQueue) Rear() int {
+/* func (m *MyCircularQueue) Rear() int {
 	if len(m.queue) == 0 {
 		return -1
 	}
 	return m.queue[len(m.queue)-1]
 }
 
-// IsEmpty checks whether the circular queue is empty or not. */
+// IsEmpty checks whether the circular queue is empty or not.
 func (m *MyCircularQueue) IsEmpty() bool {
 	return len(m.queue) == 0
 }
 
-// IsFull checks whether the circular queue is full or not. */
+// IsFull checks whether the circular queue is full or not.
 func (m *MyCircularQueue) IsFull() bool {
 	return len(m.queue) == m.k
-}
+} */
 
 /**
 * Your MyCircularQueue object will be instantiated and called as such:
@@ -123,4 +123,50 @@ func (m *MyCircularQueue) IsFull() bool {
 * param_6 := obj.IsFull();
  */
 
- //这只是一个普通队列，不是循环队列
+//这只是一个普通队列，不是循环队列 */
+package main
+
+var (
+	nums []int
+	lens = 10
+	f    = 0
+	r    = 0
+)
+
+func init() {
+	nums = make([]int, lens)
+}
+
+func full() bool {
+	return f == (r+1)%lens
+}
+
+func empity() bool {
+	return f == r
+}
+func pop() (x int, ok bool) {
+	if empity() {
+		ok = false
+	}
+	x = nums[f]
+	f = (f + 1) % lens
+	ok = true
+	return
+}
+func push(x int, ok bool) {
+	if full() {
+		ok = false
+		return
+	}
+	nums[r] = x
+	r = (r + 1) % lens
+	ok = true
+}
+
+//循环意义下的左闭右开区间，[f,r),少用一个空间，装不满
+//代码优化，%取模慢，代替方法：如果 ++w， w == lens, w = 0;
+//规避取模运算，判断条件 f== (r+1)%n 替换为  (r+1) < lens? f == (r+1); f==0
+//本质：抽屉原理，固定r,f有lens种取值，表达lens种状态，但其实有lens+1种状态
+//这样就不会一一对应，两种取值对应一个取值就会引起冲突
+//以上方法，就是少装一个元素，来给判断空or满留足情况
+//或者利用一个计数器来，利用额外信息存储工具 记录队列全部信息表达
