c01522666e1ce59321362c005cbbffc4890df97f
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-01-16 09:22:53 +0800

    优化基数排序代码

--- src/Sort/Radix Sort/main.go
+++ src/Sort/Radix Sort/main.go
@@ -3,7 +3,10 @@
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type myQueue struct {
 	first *myQueueNode
@@ -34,7 +37,7 @@ func (queue *myQueue) Push(n int) {
 	queue.end = newNode
 }
 
-//这里设计错误，队列是先进先出，应该Pop队首元素
+//刚开始这里设计错误，队列是先进先出，应该Pop队首元素
 func (queue *myQueue) Pop() (q int, ok bool) {
 
 	if queue.first == nil {
@@ -50,40 +53,30 @@ func (queue *myQueue) Pop() (q int, ok bool) {
 func main() {
 	nums := []int{31, 99, 7, 56, 24, 38, 91, 17, 76, 39}
 	radix := [10]myQueue{}
-	//先按照个位，依次存入对应的队列中
-	for _, v := range nums {
-		radix[v%10].Push(v)
-	}
-	//按照顺序从每个队列中依次取出
-	nums = nil
-	for i := 0; i < 10; i++ {
-		for {
-			q, ok := radix[i].Pop()
-			if ok == false {
-				break
-			}
-			nums = append(nums, q)
-		}
-	}
 
-	//先按照十位，依次存入对应的队列中
-	radix = [10]myQueue{}
-	for _, v := range nums {
-		radix[v/10].Push(v)
-	}
-	//按照顺序从每个队列中依次取出
-	nums = nil
-	for i := 0; i < 10; i++ {
-		for {
-			q, ok := radix[i].Pop()
-			if ok == false {
-				break
-			}
-			nums = append(nums, q)
+	for k := 0; k < 2; k++ {
+		kk := int(math.Pow10(k))
+
+		//第一次先按照个位,第二次按照十位，依次存入对应的队列中
+		for _, v := range nums {
+			radix[v/kk%10].Push(v)
 		}
+		//按照顺序从每个队列中依次取出
+		nums = nil
+		for i := 0; i < 10; i++ {
+			for {
+				q, ok := radix[i].Pop()
+				if ok == false {
+					break
+				}
+				nums = append(nums, q)
+			}
+		}
+		radix = [10]myQueue{}
 	}
 
 	fmt.Println(nums)
 }
 
+//刚开始队列判断是否为空没有实现，代码冗余；个位和十位的处理应该进一步抽象统一，做成循环
 //可以扩展，增加位数，也可以把其他排序问题，转换为数字，再运用基数排序思想排序
