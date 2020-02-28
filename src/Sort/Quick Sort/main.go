f7e9e147d57d304f0e3c993bd20f1fd3a7961f37
Fcourage <fcou@FcouragedeMacBook-Air.local>
2020-02-28 11:36:07 +0800

    快速排序算法，原地置换版本，不需要额外的内存空间

--- src/Sort/Quick Sort/main.go
+++ src/Sort/Quick Sort/main.go
@@ -1,60 +1,45 @@
 /*
-append 是个坑，切片扩容丢失信息
-算法思想很简单，但数组下标判断很烦，改天再想
-解决切片下标办法，之间弃用append,自己写个需要的数组拼接函数
-以下实现比之前利用left,right下标来回移动，好理解，同时逻辑判断简单，但多用了内存空间
+快速排序算法，原地置换版本，不需要额外的内存空间
 */
 package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func QuickSort(nums []int) []int {
-	lens := len(nums)
-	if lens < 2 {
-		return nums
+func quickSort(nums []int) {
+	size := len(nums)
+	if size < 2 {
+		return
 	}
-	var less []int
-	var big []int
+	pivot := rand.Intn(size - 1)
+	nums[size-1], nums[pivot] = nums[pivot], nums[size-1]
 
-	pivot := nums[0]
-
-	for i := 1; i < lens; i++ {
-		if nums[i] <= pivot {
-			less = append(less, nums[i])
+	pivot = size - 1
+	left := 0
+	right := size - 2
+	for left <= right {
+		if nums[left] <= nums[pivot] {
+			left++
 		} else {
-			big = append(big, nums[i])
+			nums[left], nums[right] = nums[right], nums[left]
+			right--
 		}
 	}
+	nums[left], nums[pivot] = nums[pivot], nums[left]
+	pivot = left
 
-	return comberArray(comberInt(QuickSort(less), pivot), QuickSort(big))
-}
+	quickSort(nums[:pivot])
+	quickSort(nums[pivot+1:])
 
-func comberInt(a []int, b int) []int {
-
-	c := make([]int, len(a)+1)
-	for i := 0; i < len(a); i++ {
-		c[i] = a[i]
-	}
-	c[len(a)] = b
-	return c
-}
-
-func comberArray(a []int, b []int) []int {
-
-	c := make([]int, len(a)+len(b))
-	for i := 0; i < len(a); i++ {
-		c[i] = a[i]
-	}
-	for j := 0; j < len(b); j++ {
-		c[len(a)+j] = b[j]
-	}
-	return c
 }
 
 func main() {
-	nums := []int{6, 1, 3, 4, 9, 8, 2, 5, 7, 99, 47, 87, 34}
+	rand.Seed(time.Now().Unix())
+	nums := []int{3, 2, 4, 9, 7, 6, 5, 1, 8, 99, 0}
 
-	fmt.Println(QuickSort(nums))
+	quickSort(nums)
+	fmt.Println(nums)
 }
---
+++ src/Sort/Quick Sort/mainOne.go
@@ -0,0 +1,60 @@
+/*
+append 是个坑，切片扩容丢失信息
+算法思想很简单，但数组下标判断很烦，改天再想
+解决切片下标办法，之间弃用append,自己写个需要的数组拼接函数
+以下实现比之前利用left,right下标来回移动，好理解，同时逻辑判断简单，但多用了内存空间
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func QuickSort(nums []int) []int {
+	lens := len(nums)
+	if lens < 2 {
+		return nums
+	}
+	var less []int
+	var big []int
+
+	pivot := nums[0]
+
+	for i := 1; i < lens; i++ {
+		if nums[i] <= pivot {
+			less = append(less, nums[i])
+		} else {
+			big = append(big, nums[i])
+		}
+	}
+
+	return comberArray(comberInt(QuickSort(less), pivot), QuickSort(big))
+}
+
+func comberInt(a []int, b int) []int {
+
+	c := make([]int, len(a)+1)
+	for i := 0; i < len(a); i++ {
+		c[i] = a[i]
+	}
+	c[len(a)] = b
+	return c
+}
+
+func comberArray(a []int, b []int) []int {
+
+	c := make([]int, len(a)+len(b))
+	for i := 0; i < len(a); i++ {
+		c[i] = a[i]
+	}
+	for j := 0; j < len(b); j++ {
+		c[len(a)+j] = b[j]
+	}
+	return c
+}
+
+func mainOne() {
+	nums := []int{6, 1, 3, 4, 9, 8, 2, 5, 7, 99, 47, 87, 34}
+
+	fmt.Println(QuickSort(nums))
+}
