95b0a77f0537ea00e982bdb6889aac256a6fa1f4
Fcou <lyz672673107@gmail.com>
2020-07-16 18:17:03 +0800

    位图

--- classTwo/Bit_Map/bit_map.go
+++ classTwo/Bit_Map/bit_map.go
@@ -1,8 +1,11 @@
+// 将数字 A 的第 k 位设置为1：A = A | (1 << (k - 1))
+// 将数字 A 的第 k 位设置为0：A = A & ~(1 << (k - 1))
+// 检测数字 A 的第 k 位：A & (1 << (k - 1)) != 0
 package bitmap
 
 type BitMap struct {
 	bytes []byte
-	nbits int
+	nbits int //可存储的最大值
 }
 
 func newBitMap(nbits int) *BitMap {
@@ -17,7 +20,7 @@ func (b *BitMap) set(k int) {
 	}
 	byteIndex := k / 8
 	bitIndex := k % 8
-	b.bytes[byteIndex] |= (1 << bitIndex)
+	b.bytes[byteIndex] |= (1 << bitIndex) // 从0到无穷大，每8个数对应一个byte存储
 }
 
 // 判断是否存在数字k
--- classTwo/Bit_Map/bit_map_test.go
+++ classTwo/Bit_Map/bit_map_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestBitMap(t *testing.T) {
-	b := newBitMap(100)
+	b := newBitMap(50)
 	for i := 20; i <= 80; i++ {
 		b.set(i)
 	}
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 200; i++ {
 		if b.get(i) {
 			fmt.Println("存在", i)
 		} else {
