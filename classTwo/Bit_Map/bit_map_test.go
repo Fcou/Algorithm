522e0fcfd29ad581efe8e25b65514ca501bf7807
Fcou <lyz672673107@gmail.com>
2020-07-17 16:00:56 +0800

    ac

--- classTwo/Bit_Map/bit_map_test.go
+++ classTwo/Bit_Map/bit_map_test.go
@@ -6,15 +6,14 @@ import (
 )
 
 func TestBitMap(t *testing.T) {
-	b := newBitMap(50)
-	for i := 20; i <= 80; i++ {
-		b.set(i)
+	b := newBitMap(201326592)
+
+	b.set(178288549)
+
+	if b.get(178288549) {
+		fmt.Println("存在")
+	} else {
+		fmt.Println("不存在")
 	}
-	for i := 0; i < 200; i++ {
-		if b.get(i) {
-			fmt.Println("存在", i)
-		} else {
-			fmt.Println("不存在", i)
-		}
-	}
+
 }
